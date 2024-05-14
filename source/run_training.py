a11415a544db0d1b630e5c93504d3b4994866033
Alessandro Francesco Ulivi (ext) <ale.ulivi@gmail.com>
2024-05-14 10:58:54 +0200

    define run_training.py

---
+++ source/run_training.py
@@ -0,0 +1,54 @@
+#import matplotlib.pyplot as plt
+#from functools import partial
+#from itertools import product
+
+import numpy as np
+import torch
+import torch.nn as nn
+#import torch.nn.functional as F
+
+#from torch.utils.data import Dataset
+#from torch.optim.lr_scheduler import ReduceLROnPlateau
+#from torch.utils.tensorboard import SummaryWriter
+
+#import sklearn.metrics as metrics
+#from sklearn.model_selection import train_test_split
+
+#from imageio import imread
+#from tqdm import tqdm, trange
+from test import validate
+from train import train
+
+
+def run_training(model, optimizer, metric, 
+                 n_epochs, train_loader, val_loader, loss_function,
+                 log_interval, logger, device):
+    # Use the unet you expect to work the best!
+    model = model.to(device)
+
+    # use adam optimizer
+    #optimizer = torch.optim.Adam(model.parameters())
+
+    # build the dice coefficient metric
+    #metric = DiceCoefficient()
+
+    # train for n_epochs
+    # during the training inspect the
+    # predictions in the tensorboard
+    #n_epochs = 25
+    for epoch in range(n_epochs):
+        # train
+        train(
+            model,
+            train_loader,
+            optimizer=optimizer,
+            loss_function=loss_function,
+            epoch=epoch,
+            log_interval=log_interval,
+            tb_logger=logger,
+            device=device,
+        )
+        step = epoch * len(train_loader)
+        # validate
+        validate(model, val_loader, loss_function, metric, step=step, tb_logger=logger)
+
