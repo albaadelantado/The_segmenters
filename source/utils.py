e9784733022f75c5deb1da886fa9fceb086287f0
Alessandro Francesco Ulivi (ext) <ale.ulivi@gmail.com>
2024-05-15 17:34:26 +0200

    add mask_array to utils.py

--- source/utils.py
+++ source/utils.py
@@ -6,7 +6,6 @@ import os
 
 
 
-
 def load_h5(path, key = None):
 
     with h5py.File(path) as f:
@@ -137,3 +136,10 @@ def show_output_histogram(dataset,device,model):
     sy = s(torch.Tensor(y))
     plt.imshow(sy[0,...])
 
+
+def mask_array(array2mask, threshold=0.5, mask_values=(1,0), output_dtype=np.uint8, saving_folder="", saving_key="predictions"):
+    masked_array = np.where(array2mask>threshold, mask_values[0],mask_values[1]).astype(output_dtype)
+    if len(saving_folder)>0:
+        save_h5(masked_array,saving_folder, key=saving_key)
+    return masked_array
+    
