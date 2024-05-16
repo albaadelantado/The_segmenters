192977bfd7528fa21245f201bbe332926fcdd217
Milo Imbeni (ext) <milo.imbeni@lin-dl4mia-17.htechnopole.local>
2024-05-16 10:16:37 +0200

    discrete angle transform

---
+++ source/customtransform.py
@@ -0,0 +1,11 @@
+import torchvision.transforms.functional as TF
+import random
+from typing import Sequence
+
+class DiscreteRotateTransform:
+    def __init__(self, angles: Sequence[int]):
+        self.angles = angles
+
+    def __call__(self, x):
+        angle = random.choice(self.angles)
+        return TF.rotate(x, angle)
